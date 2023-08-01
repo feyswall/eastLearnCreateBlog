14c452c50a03762f0316b52096664b81426fa98d
feyswall <vicentmkota91@gmail.com>
2023-08-01 22:05:43 +0300

    then single post error is solved

--- views/createPost.php
+++ views/createPost.php
@@ -1,45 +1,21 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <?php
-        include_once "../_partials/_head.php";
-        ?>
-        <title>index | page</title>
-    </head>
-    <body>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>index | page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mt-3">
-        <?php
-        include_once "../_partials/_navigation.php";
-        ?>
-    </nav>
+<nav class="navbar navbar-expand-lg navbar-light bg-light mt-3">
+    <?php
+        include_once "views/_partials/_navigation.php";
+    ?>
+</nav>
 
     <div class="container mt-5">
-
-        <?php
-
-        if ( isset($_GET['status']) ){
-            if ( $_GET['status'] == 'success'){
-                ?>
-                <div class="alert alert-success">
-                    <span><?php echo $_GET['message']; ?></span>
-                </div>
-                <?php
-            }
-        }
-
-        if ( isset($_GET['status']) ){
-            if ( $_GET['status'] == 'error'){
-                ?>
-                <div class="alert alert-success">
-                    <span><?php echo $_GET['message']; ?></span>
-                </div>
-                <?php
-            }
-        }
-
-        ?>
-
 
        <div class="card">
            <div class="card-body">
