e0ee17947a5b7de9019a38128a5b8fa5ebcd1f21
feyswall <vicentmkota91@gmail.com>
2023-08-05 07:15:34 +0300

    episod 51 mini blog

--- backEnd/addPost.php
+++ backEnd/addPost.php
@@ -5,7 +5,7 @@ include_once __DIR__."/../controller/config.php";
 $conn = db_connection();
 
 if ( !$conn ){
-    header("location: createPost.php");
+    header("location: ../views/createPost.php");
     exit();
 }
 
@@ -19,14 +19,47 @@ if( isset($_POST['send'])){
     
     if( is_array($photo) ){
         if( count($photo) < 1){
-            header("location: createPost.php");
+            header("location: ../views/createPost.php");
             exit();
         }else{
-            echo "endelea";
+            // Upload image file
+            $fileName = $photo['name'];
+            $fileTmpName = $photo['tmp_name'];
+
+            $fileSize = $photo['size'];
+            $fileError = $photo['error'];
+            $fileType = $photo['type'];
+
+            $resultArray = explode('.', $fileName);
+            $fileExtension = strtolower(end($resultArray));
+
+            $imageExtension = array('png', 'jpg', 'jpeg', 'webp');
+
+            if (in_array($fileExtension, $imageExtension)) {
+                if ($fileSize <= 2000000) {
+                    if ($fileError !== 0) {
+                        echo "File init Error was Found";
+                    } else {
+                        $fileNewName = uniqid('', true) . '.' . $fileExtension;
+                        $destination =  __DIR__."/../public/posts/". $fileNewName;
+                        $move_file = move_uploaded_file($fileTmpName, $destination);
+                        if ( $move_file ) {
+                            echo $fileNewName;
+                        } else {
+                            echo "Fail to move the Uploaded File";
+                        }
+                    }
+                } else {
+                    echo "File Size Too Large...";
+                }
+            } else {
+                echo "File Uploaded is not an image";
+            }
+            // save data katika database
         }
     }
 
 } else{
-    header("location: createPost.php");
+    header("location: ../views/createPost.php");
     exit();
 }
